2ff3c97447da10b8691685d7dfc88b406f4bf632
Makhayev <y.makhayev@alabs.team>
2022-10-20 13:30:36 +0600

    fix: added dotenv and scripts

--- server.ts
+++ server.ts
@@ -1,6 +1,8 @@
 import Express from 'express';
 const app = Express();
 const port = 3002;
+import * as dotenv from 'dotenv';
+dotenv.config()
 
 app.get('/', (req, res) => {
     res.send('Hello World!');
